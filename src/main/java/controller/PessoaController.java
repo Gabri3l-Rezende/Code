3787ba86bace2b7ccca3887b7a7c51277eee7f76
Gustavo <gustavolopes159@hotmail.com>
2024-10-12 09:06:33 -0300

    Gentileza baixar esse repositorio

---
+++ src/main/java/controller/PessoaController.java
@@ -0,0 +1,7 @@
+package controller;
+
+/**O Controller lida com as requisições e respostas
+ (geralmente uma interface gráfica ou API) e a lógica de negócio. **/
+
+public class PessoaController {
+}
---
+++ src/main/java/service/PatternApplication.java
@@ -0,0 +1,11 @@
+package service;
+
+public class PatternApplication {
+
+    /**O Service encapsula a lógica de negócio da aplicação. Ele é responsável por manipular as regras de negócio de
+    maneira desacoplada do Controller e do Repository**/
+
+    public static void main(String[] args) {
+
+    }
+}
